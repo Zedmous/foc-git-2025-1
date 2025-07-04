f53760f55d9a0fbee5bef8ee7d37bd8fe28f9091
Zedmous <59229407+Zedmous@users.noreply.github.com>
2025-07-04 03:03:33 -0400

    operaciones en app

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("Este es el primer mensaje de la aplicación");
+sumar = (a, b) => {
+  return a + b;
+};
+
+restar = (a, b) => {
+  return a - b;
+};
+
+multiplicar = (a, b) => {
+  return a * b;
+};
+
+dividir = (a, b) => {
+  if (b === 0) {
+    throw new Error("No se puede dividir por cero");
+  }
+  return a / b;
+};
+
+console.log("Operaciones matemáticas definidas");
+console.log(`La suma de 5 y 3 es: ${sumar(5, 3)}`);
+console.log(`La resta de 5 y 3 es: ${restar(5, 3)}`);
+console.log(`La multiplicación de 5 y 3 es: ${multiplicar(5, 3)}`);
+console.log(`La división de 5 y 3 es: ${dividir(5, 3)}`);
