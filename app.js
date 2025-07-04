aabd86a7f195e1bc1dd6eb74e301232f35dd71f0
Zedmous <59229407+Zedmous@users.noreply.github.com>
2025-07-04 03:07:01 -0400

    operaciones en el app de multiplicar

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const { Calculadora } = require("./clases/calculadora");
+
 console.log("Este es el primer mensaje de la aplicación");
+const calculadora = new Calculadora();
+let multiplicar = calculadora.multiplicar(5, 10);
+console.log(`El resultado de la multiplicación es de 5 * 10 es: ${multiplicar}`);
